1404c1b8a101fdf3472a021dc2880c0469ddcfb5
janarthanan.s <chandrunair44@gmail.com>
2022-09-04 17:18:10 +0530

    commit

--- JS/todo.js
+++ JS/todo.js
@@ -1,7 +1,46 @@
-let todo = angular.module("myTodo",[]);
+var todo = angular.module("myTodo",["ui.router"]);
 
-todo.controller("myCtrl",function($scope){
-    var workers = [
+todo.config(["$stateProvider",function($stateProvider,$locationProvider,$urlRouterProvider){
+    $stateProvider
+    .state('page2', {
+        url: '/page2',
+        templateUrl :"page2.html",
+        controller : "page1controller"
+    })
+    .state("page1",{
+        url: "/page1",
+        templateUrl: "page1.html",
+        controller: "myCtrl",
+    });
+    // $urlRouterProvider.otherwise('/')
+}]);
+
+todo.controller("myCtrl",function($scope,$state){
+    $scope.secondFunction = function(){
+        $state.go("page1");
+    };
+});
+
+todo.controller("page1controller",function($scope,$state){
+    $scope.firstFunction = function(){
+        $state.go("page2");
+    }
+})
+todo.controller("page1controller",function($scope){
+        let names = [
+            "Ashiq",
+            "Ashik",
+            "Janarthanan",
+            "Karunakaran",
+            "Manoj",
+            "Vignesh",
+            "Yuvan"
+        ];
+        $scope.names = names;
+});
+
+todo.controller("page2controller",function($scope,$state){
+    let workers = [
         {
             sno: 1,
             assigned:"Ashik",
@@ -47,4 +86,9 @@ todo.controller("myCtrl",function($scope){
     ];
     $scope.workers = workers;
 });
+
+
+
+
+
 
